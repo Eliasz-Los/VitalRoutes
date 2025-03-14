35a76a734309e4b60e7d4c12d16b135325568ba9
marcin <marcin.bujas@student.kdg.be>
2025-03-14 09:04:35 +0100

    forgor to add the files oopsie

---
+++ ui/lib/Services/KalmanFilter.dart
@@ -0,0 +1,26 @@
+﻿class KalmanFilter {
+  late double _q; // Process noise covariance
+  late double _r; // Measurement noise covariance
+  late double _x; // Value
+  late double _p; // Estimation error covariance
+  late double _k; // Kalman gain
+
+  KalmanFilter({double q = 0.001, double r = 0.1, double initialValue = 0.0}) {
+    _q = q;
+    _r = r;
+    _x = initialValue;
+    _p = 1.0;
+  }
+
+  double filter(double measurement) {
+    // Prediction update
+    _p = _p + _q;
+
+    // Measurement update
+    _k = _p / (_p + _r);
+    _x = _x + _k * (measurement - _x);
+    _p = (1 - _k) * _p;
+
+    return _x;
+  }
+}
---
+++ ui/lib/Services/ScalingService.dart
@@ -0,0 +1,11 @@
+﻿class ScalingService{
+  static double scaleY(double y) {
+    double scaled = (y / 7.46) * 0.65;
+    return scaled;
+  }
+  
+  static double scaleX(double x) {
+    double scaled = x / 10.77;
+    return scaled;
+  }
+}
