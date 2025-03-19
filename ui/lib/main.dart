8c23f1b3249f14f3ee84c9990dbc5ea1300e54ea
marcin <marcin.bujas@student.kdg.be>
2025-03-19 17:08:23 +0100

    errara

--- ui/lib/main.dart
+++ ui/lib/main.dart
@@ -17,7 +17,6 @@ void main() async {
   String e1 = ".env.development";
   String e2 = ".env.production";
   String envFile = String.fromEnvironment('ENV_FILE', defaultValue: e2);
-  print('Using env file: $envFile');
   await dotenv.load(fileName: envFile);
   runApp(MyApp(userProvider: UserProvider()));
 }
